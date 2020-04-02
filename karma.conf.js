4504658ccabd40d275200c3edab4e1b00b7f31ee
hugoj-goncalves <hugo159@gmail.com>
2020-04-02 12:23:13 -0300

    Revert

--- karma.conf.js
+++ karma.conf.js
@@ -58,10 +58,6 @@ module.exports = function (config) {
           availableBrowsers[index] = 'ChromiumHeadlessNoSandbox';
         }
 
-        if (availableBrowsers.includes('IE')) {
-            availableBrowsers = availableBrowsers.filter(e => e != 'IE');
-        }
-
         return availableBrowsers;
       }
     },
